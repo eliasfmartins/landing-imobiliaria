990a8b8ff459efd0cf4162e9c1497247bfbbc103
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-03-11 18:02:21 -0300

    first commit

---
+++ src/app/components/header/index.tsx
@@ -0,0 +1,17 @@
+export const Header = () => {
+    return (
+        <header className="bg-zinc-700">
+            <div className="mx-auto max-w-[1200px] bg-zinc-500">
+                Logo Here
+                <nav>
+                    <ul>
+                        <li>Opcao1</li>
+                        <li>Opcao1</li>
+                        <li>Opcao1</li>
+                        <li>Opcao1</li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+}
