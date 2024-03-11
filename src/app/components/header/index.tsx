2248e56504a2cc7fa2d46fba33972c48cd9388c8
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-03-11 18:43:14 -0300

    feat: base files and header

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,14 +1,16 @@
 export const Header = () => {
     return (
-        <header className="bg-zinc-700">
-            <div className="mx-auto max-w-[1200px] bg-zinc-500">
+        <header className="bg-zinc-100">
+            <div className="mx-auto max-w-[1200px] bg-zinc-100 flex justify-between py-4 items-center text-gray-500">
                 Logo Here
                 <nav>
-                    <ul>
-                        <li>Opcao1</li>
-                        <li>Opcao1</li>
-                        <li>Opcao1</li>
-                        <li>Opcao1</li>
+                    <ul className="flex gap-4">
+                        <li className="border-b-4 border-transparent hover:border-cyan-400 transition duration-500 p-2 ">Opcao1</li>
+                        <li className="border-b-4 border-transparent hover:border-cyan-400 transition duration-500 p-2 ">Opcao1</li>
+                        <li className="border-b-4 border-transparent hover:border-cyan-400 transition duration-500 p-2 ">Opcao1</li>
+                        <li className="border-b-4 border-transparent hover:border-cyan-400 transition duration-500 p-2 ">Opcao1</li>
+
+
                     </ul>
                 </nav>
             </div>
