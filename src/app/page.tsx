43eb8a0fe07c4b758952fed7c4daa23888db6031
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-03-13 15:50:23 -0300

    feat: image for div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,10 @@ export default function Home() {
   return (
     <main >
       <Header />
-      <div className="relative h-[85vh] bg-gradient-to-t from-black to-transparent z-10 bg-bac" >
-        <img src="/tower.jpg" alt="Tower" className="absolute inset-0 object-cover w-full h-full z-0" />
+      <div className="" style={{ backgroundImage: 'linear-gradient(to bottom, #0001, #000), url(/tower.jpg)', filter: 'brightness(150%)', backgroundSize: 'cover', backgroundPosition: 'center', height: 'calc(100vh - 76px)' }}>
+        <h2>oi</h2>
       </div>
-      <div className="relative h-[85vh] bg-gradient-to-t from-black to-transparent z-10" >
-        <img src="/tower.jpg" alt="Tower" className="absolute inset-0 object-cover w-full h-full" />
-      </div>
-      <div className='container'>Pao</div>
+
     </main>
   );
 }
