552f080f93d08cfb5328cadf37723605c11ab815
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-03-12 21:01:25 -0300

    feat: image files and header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,13 @@ export default function Home() {
   return (
     <main >
       <Header />
-
+      <div className="relative h-[85vh] bg-gradient-to-t from-black to-transparent z-10 bg-bac" >
+        <img src="/tower.jpg" alt="Tower" className="absolute inset-0 object-cover w-full h-full z-0" />
+      </div>
+      <div className="relative h-[85vh] bg-gradient-to-t from-black to-transparent z-10" >
+        <img src="/tower.jpg" alt="Tower" className="absolute inset-0 object-cover w-full h-full" />
+      </div>
+      <div className='container'>Pao</div>
     </main>
   );
 }
