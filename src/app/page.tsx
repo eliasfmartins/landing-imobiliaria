03694892a418bfff33717cac71f016c779f1acb4
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-03-14 19:05:30 -0300

    feat: config img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,15 @@ export default function Home() {
         <div className='flex flex-col gap-y-6'>
           <h2 className={`${playfair.className} text-7xl font-[700] text-white tracking-widest`} style={{ textShadow: '5px 1px 4px rgba(0, 0, 0, 0.5)' }}>MARTINS & SILVA</h2>
           <h3 className={`${roboto.className} text-4xl font-[200] text-white tracking-widest`} style={{ textShadow: '5px 1px 4px rgba(0, 0, 0, 0.5)' }}>Escritório Imobiliário</h3>
+        </div>
+      </div>
+      <div className='min-h-[35vh] flex'>
+        <div className='flex flex-col '>
+          <h2>Um Lançamento Com <br /> Excelência E Qualidade</h2>
+
+          <p>Oportunidade única para quem tem bom gosto e quer morar bem</p>
+
+          <button className=' border border-b-gray-700 border-solid  '>Alguma coisa</button>
 
 
         </div>
