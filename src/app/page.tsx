788895dc027b55ddd724c069cf5abc1772c476ad
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-03-14 11:56:25 -0300

    feat: text shandown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,10 @@ export default function Home() {
       <Header />
       <div className="flex items-center justify-center text-center " style={{ backgroundImage: 'linear-gradient(to bottom, #0001, #000), url(/tower.jpg)', filter: 'brightness(150%)', backgroundSize: 'cover', backgroundPosition: 'center', height: 'calc(100vh - 80px)' }}>
         <div className='flex flex-col gap-y-6'>
-          <h2 className={`${playfair.className} text-7xl font-[700] text-white shadow-b-2xl shadow-indigo-500/50  tracking-widest`}>MARTINS & SILVA</h2>
-          <h3 className={`${roboto.className} text-4xl font-[200] text-white tracking-widest`}>Escritório Imobiliário</h3>
+          <h2 className={`${playfair.className} text-7xl font-[700] text-white tracking-widest`} style={{ textShadow: '5px 1px 4px rgba(0, 0, 0, 0.5)' }}>MARTINS & SILVA</h2>
+          <h3 className={`${roboto.className} text-4xl font-[200] text-white tracking-widest`} style={{ textShadow: '5px 1px 4px rgba(0, 0, 0, 0.5)' }}>Escritório Imobiliário</h3>
+
+
         </div>
       </div>
 
