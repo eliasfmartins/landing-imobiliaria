23da132ee4c7d777470b28b5ac82fe609764f000
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-03-27 17:53:04 -0300

    feat: create state for nav menu

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,20 +1,38 @@
-import Image from "next/image"
+import Image from "next/image";
+import { useState } from "react";
 
 export const Header = () => {
-    return (
-        <header className="bg-zinc-100">
-            <div className="mx-auto max-w-[1200px] bg-zinc-100 flex justify-between py-4 items-center text-gray-500 max-h-20" >
-                <Image src={'/pao.svg'} height={120} width={120} alt="" className="object-cover" />
-                <nav>
-                    <ul className="flex gap-4">
-                        <li className="border-b-4 border-transparent hover:border-cyan-400 transition duration-500 p-2 ">Home</li>
-                        <li className="border-b-4 border-transparent hover:border-cyan-400 transition duration-500 p-2 ">Venda</li>
-                        <li className="border-b-4 border-transparent hover:border-cyan-400 transition duration-500 p-2 ">Locação</li>
-                        <li className="border-b-4 border-transparent hover:border-cyan-400 transition duration-500 p-2 ">Quem Somos</li>
-                        <li className="border-b-4 border-transparent hover:border-cyan-400 transition duration-500 p-2 ">Fale Conosco</li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-    )
-}
+  const [navbar, setNavBar] = useState<boolean>(false);
+  return (
+    <header className="bg-zinc-100">
+      <div className="mx-auto max-w-[1200px] bg-zinc-100 flex justify-between py-4 items-center text-gray-500 max-h-20">
+        <Image
+          src={"/pao.svg"}
+          height={120}
+          width={120}
+          alt=""
+          className="object-cover"
+        />
+        <nav className="lg">
+          <ul className="flex gap-4">
+            <li className="border-b-4 border-transparent hover:border-cyan-400 transition duration-500 p-2 ">
+              Home
+            </li>
+            <li className="border-b-4 border-transparent hover:border-cyan-400 transition duration-500 p-2 ">
+              Venda
+            </li>
+            <li className="border-b-4 border-transparent hover:border-cyan-400 transition duration-500 p-2 ">
+              Locação
+            </li>
+            <li className="border-b-4 border-transparent hover:border-cyan-400 transition duration-500 p-2 ">
+              Quem Somos
+            </li>
+            <li className="border-b-4 border-transparent hover:border-cyan-400 transition duration-500 p-2 ">
+              Fale Conosco
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
