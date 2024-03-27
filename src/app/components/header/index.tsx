11ebeb8bb3dbc43b804486bed234c3c27a2f24a7
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-03-27 19:57:28 -0300

    feat: menu mobile

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -6,7 +6,7 @@ export const Header = () => {
   const [navbar, setNavBar] = useState<boolean>(false);
   return (
     <header className="bg-zinc-100">
-      <div className="mx-auto max-w-[1200px] bg-zinc-100 flex justify-between py-4 items-center text-gray-500 max-h-20">
+      <div className="mx-auto max-w-[1200px] bg-zinc-100 flex justify-between py-4 items-center  px-8 text-gray-500 max-h-20">
         <Image
           src={"/pao.svg"}
           height={120}
@@ -14,9 +14,9 @@ export const Header = () => {
           alt=""
           className="object-cover"
         />
-        <nav>
+        <nav className="flex">
           <ul
-            className={` items-center justify-center h-20 sm:flex bg-red-500  ${navbar ? "h-screen flex bg-purple-600 flex-col m-auto top-0 right-0 left-0 fixed z-20" : "h-0 transition-all ease-in-out"}`}
+            className={` items-center justify-center h-20 flex  w-0 sm:w-auto overflow-hidden sm:flex bg-red-500${navbar ? "flex bg-purple-600 flex-col m-auto top-0 right-0 left-0 fixed z-20 h-screen" : "h-0 transition-all ease-in-out "}`}
           >
             <li
               className="border-b-4 border-transparent hover:border-cyan-400 transition duration-500 p-2 "
@@ -50,7 +50,7 @@ export const Header = () => {
             </li>
           </ul>
           <button
-            className="sm:hidden bg-red-600 z-50"
+            className="sm:hidden bg-red-600 h-fit m-auto"
             onClick={() => setNavBar((e) => !e)}
           >
             {navbar ? "x" : "="}
