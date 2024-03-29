d3b19e9056b12071e65cec0110fd4705726c6bb1
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-03-29 18:25:29 -0300

    feat: att menu mobile

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -5,18 +5,24 @@ import { useState } from "react";
 export const Header = () => {
   const [navbar, setNavBar] = useState<boolean>(false);
   return (
-    <header className="bg-zinc-100">
+    <header className={` bg-slate-400 z-50 w-full ${navbar ? " fixed " : ""}`}>
       <div className="mx-auto max-w-[1200px] bg-zinc-100 flex justify-between py-4 items-center  px-8 text-gray-500 max-h-20">
         <Image
           src={"/pao.svg"}
           height={120}
           width={120}
           alt=""
-          className="object-cover"
+          className="object-cover z-40"
         />
         <nav className="flex">
+          <button
+            className="sm:hidden bg-red-600 h-fit m-auto z-30"
+            onClick={() => setNavBar((e) => !e)}
+          >
+            {navbar ? "x" : "="}
+          </button>
           <ul
-            className={` items-center justify-center h-20 flex  w-0 sm:w-auto overflow-hidden sm:flex bg-red-500${navbar ? "flex bg-purple-600 flex-col m-auto top-0 right-0 left-0 fixed z-20 h-screen" : "h-0 transition-all ease-in-out "}`}
+            className={` items-center justify-centertransition-all z-50 h-0 w-full transition-all flex ${navbar ? "flex fixed w-full top-[70px] right-0 bg-pink-500 flex-col h-screen items-center gap-10" : ""}`}
           >
             <li
               className="border-b-4 border-transparent hover:border-cyan-400 transition duration-500 p-2 "
@@ -49,12 +55,6 @@ export const Header = () => {
               Fale Conosco
             </li>
           </ul>
-          <button
-            className="sm:hidden bg-red-600 h-fit m-auto"
-            onClick={() => setNavBar((e) => !e)}
-          >
-            {navbar ? "x" : "="}
-          </button>
         </nav>
       </div>
     </header>
