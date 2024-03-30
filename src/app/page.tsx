186ef06e9de4b493cb713ef0d0865406be560dac
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-03-30 18:19:27 -0300

    feat: implementando localizacao

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 import { Header } from "./components/Header/index";
 import { playfair } from "./layout";
 import { roboto } from "./layout";
-import Image from "next/image";
 import { Footer } from "./components/Footer";
 
 export default function Home() {
@@ -33,7 +32,7 @@ export default function Home() {
           >
             Escritório Imobiliário
           </h3>
-          <div className="w-[300px] bg-orange-400 h-1 mx-auto rounded-lg mt-2"></div>
+          <div className="w-[300px] bg-yellow-600 h-1 mx-auto rounded-lg mt-2"></div>
         </div>
       </div>
       <div className="min-h-[35vh] xl:flex max-w-[1200px] mx-auto mt-20">
@@ -42,11 +41,12 @@ export default function Home() {
             Um Lançamento Com Excelência E Qualidade
           </h2>
           <p className="font-extralight text-md">
-            Oportunidade única para quem tem bom gosto e quer morar bem aqui em Valparaiso
+            Oportunidade única para quem tem bom gosto e quer morar bem aqui em
+            Valparaiso
           </p>
           <Link
             href=""
-            className="border-2 border-orange-200 w-fit px-8 py-3 font-[700] text-orange-200 hover:bg-orange-300 hover:text-white transition-all ease-in-out  rounded"
+            className="border-2 border-yellow-600 w-fit px-8 py-3 font-[700] text-yellow-600 hover:bg-yellow-600 hover:text-white transition-all ease-in-out  rounded"
           >
             Plantas
           </Link>
@@ -56,7 +56,19 @@ export default function Home() {
           className=" xl:mt-[-205px] z-10 bg-transparent flex flex-1"
         />
       </div>
-      <Footer/>
+      <div className="max-w-[1200px] mx-auto mb-8">
+        <h2 className="text font-[700]  text-3xl text-center mb-6">Endereço aqui</h2>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3833.655028886112!2d-47.97700482486391!3d-16.083381484598178!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9359858fb0af83a7%3A0x775b6a22cc9dc214!2sImobili%C3%A1ria%20Martins%20e%20Silva!5e0!3m2!1spt-BR!2sbr!4v1711832737732!5m2!1spt-BR!2sbr"
+          width="100%"
+          height="450"
+          style={{ border: "0" }}
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+
+      <Footer />
     </main>
   );
 }
