f53229967803257e7309199d86810d3524936808
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-03-31 14:09:49 -0300

    feat: media footer

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -6,20 +6,29 @@ import { MdEmail } from "react-icons/md";
 export const Footer = () => {
   return (
     <footer className="bg-black w-full">
-      <div className="max-w-[1200px] flex-col py-3 h-auto mx-auto gap-4 items-center sm:flex-row  min-h-20 text-yellow-600 flex justify-evenly text-sm">
-          <Link href={"#"} className="flex gap-2 items-center justify-center w-fit p-6">
+      <div className="max-w-[1200px] flex-col py-3 h-auto mx-auto gap-4 sm:gap-1 items-center sm:flex-row  min-h-20 text-yellow-600 flex justify-evenly  text-sm">
+        <Link
+          href={"#"}
+          className="flex gap-2 items-center justify-center w-fit p-6"
+        >
           <FaPhoneFlip size={22} />
-            <span>Central de Vendas - 8002-8922</span>
-          </Link>
-          <Link href={"#"} className="flex gap-2 items-center justify-center w-fit p-6">
-            <MdEmail size={25} />
-            <span>Atendimento por email - emailteste@gmail.com</span>
-          </Link>
-        
-          <Link href={"#"} className="flex gap-2 items-center justify-center w-fit p-6">
+          <span>Central de Vendas - 8002-8922</span>
+        </Link>
+        <Link
+          href={"#"}
+          className="flex gap-2 items-center justify-center w-fit p-6"
+        >
+          <MdEmail size={25} />
+          <span>Atendimento por email - emailteste@gmail.com</span>
+        </Link>
+
+        <Link
+          href={"#"}
+          className="flex gap-2 items-center justify-center w-fit p-6"
+        >
           <FaWhatsapp size={25} />
-            <span>Whatsapp - 61 9 0000</span>
-          </Link>
+          <span>Whatsapp - 61 9 0000</span>
+        </Link>
       </div>
     </footer>
   );
