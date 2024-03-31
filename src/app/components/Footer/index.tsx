fb8f2b070dc9bcae89b93a3389c723dca2b2b577
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-03-31 11:41:21 -0300

    feat: ajustando footer media queris

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -6,17 +6,17 @@ import { MdEmail } from "react-icons/md";
 export const Footer = () => {
   return (
     <footer className="bg-black w-full">
-      <div className="max-w-[1200px] py-3 h-[80px] mx-auto text-yellow-600 flex justify-evenly text-sm">
-          <Link href={"#"} className="flex gap-2 items-center justify-center w-fit">
+      <div className="max-w-[1200px] flex-col py-3 h-auto mx-auto gap-4 items-center sm:flex-row  min-h-20 text-yellow-600 flex justify-evenly text-sm">
+          <Link href={"#"} className="flex gap-2 items-center justify-center w-fit p-6">
           <FaPhoneFlip size={22} />
             <span>Central de Vendas - 8002-8922</span>
           </Link>
-          <Link href={"#"} className="flex gap-2 items-center justify-center w-fit">
+          <Link href={"#"} className="flex gap-2 items-center justify-center w-fit p-6">
             <MdEmail size={25} />
             <span>Atendimento por email - emailteste@gmail.com</span>
           </Link>
         
-          <Link href={"#"} className="flex gap-2 items-center justify-center w-fit">
+          <Link href={"#"} className="flex gap-2 items-center justify-center w-fit p-6">
           <FaWhatsapp size={25} />
             <span>Whatsapp - 61 9 0000</span>
           </Link>
