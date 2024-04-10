18b4e0300d2d987ce48e3611b96a610cbd6e897c
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-04-10 11:24:51 -0300

    feat: data card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -216,9 +216,18 @@ export default function Home() {
           Conheça nosso time de corretores especializados.
         </p>
         <div className="flex flex-col sm:flex-row   bg-pink-500 w-full  max-w-[600px] gap-8 justify-center mx-auto p-8">
-          
-          <CorretorCard creci="78545-DF"name="Carla Martins"/>
-          <CorretorCard creci="78545-DF"name="Carla Martins"/>
+          <CorretorCard
+            creci="78545-DF"
+            name="Carla Martins"
+            email="Carlinha Martins"
+            phone="5145415154"
+          />
+          <CorretorCard
+            creci="78545-DF"
+            name="Carla Martins"
+            email="Carlinha Martins"
+            phone="5145415154"
+          />
         </div>
       </div>
 
