e6514e44f365f8bd27a8c3978b0db9cb9ed4e064
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-04-11 20:10:15 -0300

    feat: att endereco

--- src/app/components/CardMap/index.tsx
+++ src/app/components/CardMap/index.tsx
@@ -4,7 +4,8 @@ export const CardMap = () => {
   return (
     <div className="max-w-[90%] mx-auto mb-8 sm:max-w-[1200px]">
       <h2 className="text font-[700]  text-3xl text-center mb-6">
-        Endereço aqui
+        Quadra 02 lote 01 sobre loja 02 etapa A , HZ Brasil Empresarial,
+        Valparaíso de Goiás
       </h2>
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3833.655028886112!2d-47.97700482486391!3d-16.083381484598178!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9359858fb0af83a7%3A0x775b6a22cc9dc214!2sImobili%C3%A1ria%20Martins%20e%20Silva!5e0!3m2!1spt-BR!2sbr!4v1711832737732!5m2!1spt-BR!2sbr"
