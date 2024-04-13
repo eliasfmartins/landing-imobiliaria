8308d1242c7eaaa088379c1cf2f84b50a807e6ee
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-04-13 17:23:13 -0300

    style: att font component

--- src/app/components/TitlePage/index.tsx
+++ src/app/components/TitlePage/index.tsx
@@ -1,16 +1,19 @@
+import { playfair } from "@/app/fonts";
 import React from "react";
 type TitlePageProps = {
   firstTitle: string;
-  subTitle: string;
+  subTitle?: string;
 };
 export const TitlePage = ({ firstTitle, subTitle }: TitlePageProps) => {
   return (
-    <div>
-      <div className=" max-w-[1200px] mx-auto mt-12">
-        <h2 className=" flex  justify-center text-4xl">{firstTitle}</h2>
+    <div className={`${playfair.className}`}>
+      <div className=" max-w-[1200px] mx-auto mt-5">
+        <h2 className=" flex  justify-center text-4xl text-center">
+          {firstTitle}
+        </h2>
         <hr className="w-[20%] h-1 bg-yellow-600 mx-auto  rounded-full mt-8" />
-        <hr className="w-[15%] h-1 bg-yellow-600 mx-auto  rounded-full my-3" />
-        <p className="my-5 text-center">{subTitle}</p>
+        <hr className="w-[15%] h-1 bg-yellow-600 mx-auto mb-6  rounded-full my-3" />
+        {subTitle && <p className="my-5 text-center">{subTitle}</p>}
       </div>
     </div>
   );
