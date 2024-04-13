d0c3ad63ea3087526296fc7f591b1d0344ec72d0
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-04-13 17:20:08 -0300

    feat: divisao components and links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import Link from "next/link";
 import { Header } from "./components/Header/index";
 import { playfair } from "./fonts";
 import { roboto } from "./fonts";
 import { Footer } from "./components/Footer";
-import Image from "next/image";
 import { TitlePage } from "./components/TitlePage";
 import { CardMap } from "./components/CardMap";
 import { CorretoresSection } from "./components/CorretoresSection";
 import { LocalSection } from "./components/LocalSection";
 import { CardsServices } from "./components/CardsServices";
 import { ImoveisSection } from "./components/ImoveisSection";
+import { About } from "./components/About";
+import { InfoSection } from "./components/InfoSection";
 
 export default function Home() {
   return (
@@ -25,6 +25,7 @@ export default function Home() {
           backgroundPosition: "center",
           height: "calc(100vh )",
         }}
+        id="inicio"
       >
         <div className="flex flex-col gap-y-6">
           <h2
@@ -42,50 +43,36 @@ export default function Home() {
           <div className="w-[300px] bg-yellow-700 h-1 mx-auto rounded-lg mt-2"></div>
         </div>
       </div>
-      <div className="min-h-[35vh] xl:flex max-w-[1200px] mx-auto mt-20">
-        <div className="flex flex-col space-y-10  mt-7  p-4  h-fit xl:items-start items-center">
-          <h2 className={`${playfair.className} w-96 text-3xl font-[700]`}>
-            Um Lançamento Com Excelência E Qualidade
-          </h2>
-          <p className="font-extralight text-md">
-            Oportunidade única para quem tem bom gosto e quer morar bem aqui em
-            Valparaiso
-          </p>
-          <Link
-            href=""
-            className="border-2 border-yellow-600 w-fit px-8 py-3 font-[700] text-yellow-600 hover:bg-yellow-600 hover:text-white transition-all ease-in-out  rounded"
-          >
-            Plantas
-          </Link>
-        </div>
-        <Image
-          width={1200}
-          height={1200}
-          alt=""
-          src={"/temp.png"}
-          className=" xl:mt-[-205px] z-10 bg-transparent flex flex-1"
-        />
+      <div id="about" className="pt-[80px]">
+        <About />
       </div>
       <div className=" max-w-[1200px] mx-auto mt-12">
-        <TitlePage
-          firstTitle="Imóveis em destaque?"
-          subTitle=" Os melhores imóveis á venda você encontra aqui"
-        />
-       <ImoveisSection/>
-        <TitlePage
-          firstTitle="O que nós fazemos?"
-          subTitle="Veja alguns dos nossos serviços"
-        />
+        <div id="vendaaa" className="pt-[80px]">
+          <TitlePage
+            firstTitle="Imóveis em destaque"
+            subTitle=" Os melhores imóveis á venda você encontra aqui"
+          />
+          <ImoveisSection />
+        </div>
+        <LocalSection />
+        <div id="servicos" className="pt-[80px]">
+          <TitlePage
+            firstTitle="O que nós fazemos?"
+            subTitle="Veja alguns dos nossos serviços"
+          />
+        </div>
       </div>
-      <CardsServices/>
-      <LocalSection />
+      <CardsServices />
 
       <TitlePage
         firstTitle="Nossos Corretores"
         subTitle="Conheça nosso time de corretores especializados"
       />
       <CorretoresSection />
-      <CardMap />
+      <div className="pt-[80px]" id="ondeficamos">
+        <CardMap />
+      </div>
+      <InfoSection />
       <Footer />
     </main>
   );
