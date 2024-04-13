d4b700feda49bbb8fa6292665dd4657c8a020284
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-04-13 17:23:34 -0300

    feat: att data component

--- src/app/components/LocalSection/index.tsx
+++ src/app/components/LocalSection/index.tsx
@@ -1,43 +1,47 @@
-import React from 'react'
+import React from "react";
+import { TitlePage } from "../TitlePage";
 
 export const LocalSection = () => {
   return (
-    <div className=" bg-red-400 w-full max-w-[1200px] mx-auto p-5">
-    <h2 className=" text-2xl mb-5">Imóveis por região</h2>
-    <div className=" flex w-full justify-between ">
-      <ul>
-        <h3>Valparaiso-GO</h3>
-        <li>ex</li>
-        <li>ex</li>
-        <li>ex</li>
-        <li>ex</li>
-        <li>ex</li>
-      </ul>
-      <ul>
-        <h3>Brasilia-DF</h3>
-        <li>ex</li>
-        <li>ex</li>
-        <li>ex</li>
-        <li>ex</li>
-        <li>ex</li>
-      </ul>
-      <ul>
-        <h3>Ocidental-GO</h3>
-        <li>ex</li>
-        <li>ex</li>
-        <li>ex</li>
-        <li>ex</li>
-        <li>ex</li>
-      </ul>
-      <ul>
-        <h3>Jardin Inga-GO</h3>
-        <li>ex</li>
-        <li>ex</li>
-        <li>ex</li>
-        <li>ex</li>
-        <li>ex</li>
-      </ul>
+    <div className=" w-full max-w-[1200px] mx-auto p-5">
+      <TitlePage
+        firstTitle="Imóveis por região"
+        // subTitle="A localização de alguns dos nossos imóveis temos imoveis"
+      />
+      <div className=" flex w-full justify-between flex-wrap gap-8 my-12 ">
+        <ul className="min-w-[250px] w-[90%] sm:w-[20%] text-center flex flex-col gap-3">
+          <h3 className="text-lg font-[500]">Valparaiso-GO</h3>
+          <li>Ceu Azul</li>
+          <li>Valparaiso I</li>
+          <li>Valparaiso II</li>
+          <li>Cruzeiro do Sul</li>
+          <li>Cidade Jardims</li>
+        </ul>
+        <ul className="min-w-[250px] w-[90%] sm:w-[20%] text-center flex flex-col gap-3">
+          <h3 className="text-lg font-[500]">Brasilia-DF</h3>
+          <li>Asa Norte</li>
+          <li>Noroeste</li>
+          <li>Asa Sul</li>
+          <li>Lago Norte</li>
+          <li>Park Way</li>
+        </ul>
+        <ul className="min-w-[250px] w-[90%] sm:w-[20%] text-center flex flex-col gap-3">
+          <h3 className="text-lg font-[500]">Ocidental-GO</h3>
+          <li>Parque Nápolis</li>
+          <li>Parque Nova Friburgo</li>
+          <li>Jardim ABC</li>
+          <li>Ocidental Parque</li>
+          <li>Morada das Garças</li>
+        </ul>
+        <ul className="min-w-[250px] w-[90%] sm:w-[20%] text-center flex flex-col gap-3">
+          <h3 className="text-lg font-[500]">Jardin Inga-GO</h3>
+          <li>Jardim Central</li>
+          <li>Jardim América</li>
+          <li>Jardim Planalto</li>
+          <li>Jardim Zuleika</li>
+          <li>Chácaras Marajoara</li>
+        </ul>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
