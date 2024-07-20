a0ab09119921c7440dd2b18d3f3d7d0f55628d87
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-07-20 15:09:51 -0300

    feat: removendo fontes nao usadas

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -9,13 +9,3 @@ export const inter = Cormorant_Infant({
 	weight: '400',
 	style: 'normal',
 });
-
-export const robotinho = Roboto({ subsets: ['latin'], weight: '400' });
-export const playfair = Cormorant_Unicase({
-	subsets: ['latin'],
-	weight: ['300', '500', '700'],
-});
-export const roboto = Cormorant_Infant({
-	subsets: ['latin'],
-	weight: ['300', '400', '500', '600', '700'],
-});
